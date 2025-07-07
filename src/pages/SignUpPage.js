83482ef3439819c01059c4a20ca87d8520e6399d
Beliar24 <blackdid7@gmail.com>
2025-07-07 21:56:58 +0300

    add steps for tests

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,4 +1,4 @@
-import { expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 export class SignUpPage {
   constructor(page) {
@@ -11,26 +11,38 @@ export class SignUpPage {
   }
 
   async open() {
-    await this.page.goto('/user/register');
+    await test.step(`Open page`, async () => {
+      await this.page.goto('/user/register');
+    });
   }
 
   async fillUsernameField(username) {
-    await this.usernameField.fill(username);
+    await test.step(`Filling username field`, async () => {
+      await this.usernameField.fill(username);
+    });
   }
 
   async fillEmailField(email) {
-    await this.emailField.fill(email);
+    await test.step(`Filling email field`, async () => {
+      await this.emailField.fill(email);
+    });
   }
 
   async fillPasswordField(password) {
-    await this.passwordField.fill(password);
+    await test.step(`Filling password field`, async () => {
+      await this.passwordField.fill(password);
+    });
   }
 
   async clickSignUpButton() {
-    await this.signUpButton.click();
+    await test.step(`Signup button clicked`, async () => {
+      await this.signUpButton.click();
+    });
   }
 
   async assertErrorMessageContainsText(messageText) {
-    await expect(this.errorMessage).toContainText(messageText);
+    await test.step(`Assert errorMessage contains ${messageText}`, async () => {
+      await expect(this.errorMessage).toContainText(messageText);
+    });
   }
 }
